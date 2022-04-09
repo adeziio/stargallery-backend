99953c3d0552c33092a44e85b8472ab1a047ddb8
Aden <adeziio@yahoo.com>
2022-04-09 14:52:06 -0400

    testing deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var router = require('./src/router');
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server is runing on port ${PORT}`));
 app.use("/", router);
--- src/router.js
+++ src/router.js
@@ -8,23 +8,8 @@ router.get('/listGallery', (req, res) => {
 
 })
 
-router.get('/add', jsonParser, (req, res) => {
-    // if (req.query.item) {
-    //     Data.add(req.query.item);
-    //     res.json({
-    //         status: "Success"
-    //     })
-    // }
-    // else {
-    //     res.json({
-    //         status: "Failed",
-    //         message: "Please provide a parameter value for the key item"
-    //     })
-    // }
-})
-
 // POST
-router.post('/add', jsonParser, (req, res) => {
+router.post('/upload', (req, res) => {
     // if (req.body.item) {
     //     Data.add(req.body.item);
     //     res.json({
@@ -37,6 +22,9 @@ router.post('/add', jsonParser, (req, res) => {
     //         message: "Please provide a parameter value for the key item"
     //     })
     // }
+    res.json({
+        status: "Success"
+    });
 })
 
 module.exports = router;
