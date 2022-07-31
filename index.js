80dc8a86352f65b5431542d9209e8d10e111e452
Aden <adeziio@yahoo.com>
2022-07-31 14:26:21 -0400

    modify cors config

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
 var router = require('./src/router');
 var PORT = process.env.PORT || 8080;
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.listen(PORT, () => console.log(`Server is runing on port ${PORT}`));
 app.use("/", router);
